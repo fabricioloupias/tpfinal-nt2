6268ac030a75410b993f7323f748888a8445790a
Fabricio Loupias <loupias.fabri@gmail.com>
2020-07-16 19:49:04 -0300

    app final

---
+++ src/components/FilterInput.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="">
+    <input v-model="message" placeholder="Mensaje" />
+    <p>El mensaje es: {{ message | replace | toUpperCase}}</p>
+
+    <h4>Preguntas</h4>
+    <p>
+        1 - A
+    </p>
+    <p>
+        2 - B
+    </p>
+    <p>
+        3 - D
+    </p>
+    <p>
+        4 - A, B
+    </p>
+    <p>
+        5 - B
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FilterInput',
+  filters: {
+    toUpperCase: function (value) {
+      if (!value) return ''
+      value = value.toString()
+      return value.toUpperCase()
+    },
+    replace: function (value) {
+      const chars = {
+        a: 'u',
+        e: 'o',
+        o: 'e',
+        u: 'a'
+      }
+
+      return value.replace(/[aeou]/g, function (c) { return chars[c] })
+    }
+  },
+  data () {
+    return {
+      message: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FilterInput />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import FilterInput from '@/components/FilterInput.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    FilterInput
   }
 }
 </script>
